b9c8932782a61bad49ac0f86ec1eaf359bcb95c5
anupchand <anup.chand.opt@gmail.com>
2022-01-09 14:57:01 +0545

    updating some functionality
    Still working

--- calculator.js
+++ calculator.js
@@ -1,147 +1,118 @@
 "use strict";
 
+// required variables
 let activeButton;
 let activeOpr;
 let problem;
-let oprCount;
-let hLength;
 let hasDecimal;
+let ansCount = 0;
+let oprCount = 0;
 
 const mainLcd = document.getElementById("lcdScreen");
 const historyScreen = document.getElementById("history");
-hLength = historyScreen.innerText.length;
-let ansCount = 0;
+let hLength = historyScreen.innerText.length;
 
-let checkdecimal = function () {
-  if (mainLcd.innerText.includes(`.`)) {
-    hasDecimal === true;
+// check if number has decimal
+let checkDecimal = function () {
+  if (mainLcd.innerText.includes(".")) {
+    hasDecimal = true;
   } else {
-    hasDecimal === false;
+    hasDecimal = false;
   }
 };
 
-// function to call clear screen - the clear button function cant be called somehow still learning assign independent function to click event
+// Function to clear screen ==========
 const clearThyScreen = function () {
-  mainLcd.innerText = `0`;
-  historyScreen.innerText = `0`;
+  mainLcd.innerText = "0";
+  historyScreen.innerText = "0";
   ansCount = 0;
-  hLength = historyScreen.innerText.length;
   oprCount = 0;
   hasDecimal = false;
 };
 
-// Clear button function
-document.getElementById("clearButton").onclick = function clearLcd() {
+// Clear button "C" Function ==========
+document.getElementById("clearButton").onclick = function () {
   clearThyScreen();
-  // mainLcd.innerText = `0`;
-  // historyScreen.innerText = `0`;
-  // ansCount = 0;
-  // hLength = historyScreen.innerText.length;
-  // hasDecimal = false;
 };
 
+// Input values from number keys ==========
 function takeNumbers(activeID) {
   activeButton = document.getElementById(activeID);
-  checkdecimal();
+  checkDecimal();
+
   if (ansCount === 0) {
     if (mainLcd.innerText === "0" || mainLcd.innerText === "Welcome") {
       mainLcd.innerText = activeButton.innerText;
     } else {
       mainLcd.insertAdjacentText("beforeend", activeButton.innerText);
     }
-  } else if (ansCount === 1 && oprCount === 0 && !hasDecimal) {
-    clearThyScreen();
-    mainLcd.innerText = activeButton.innerText;
-  } else if (ansCount === 1 && oprCount === 0 && hasDecimal) {
-    mainLcd.insertAdjacentText("beforeend", activeButton.innerText);
-  } else if (
-    ansCount === 1 &&
-    oprCount === 1 &&
-    Number(mainLcd.innerText) === 0
-  ) {
-    mainLcd.innerText = activeButton.innerText;
-  } else if (
-    ansCount === 1 &&
-    oprCount === 1 &&
-    Number(mainLcd.innerText) !== 0
-  ) {
-    mainLcd.insertAdjacentText("beforeend", activeButton.innerText);
+  } else if (ansCount === 1) {
+    if (oprCount === 0 && !hasDecimal) {
+      mainLcd.innerText = activeButton.innerText;
+    } else if (oprCount === 0 && hasDecimal) {
+      mainLcd.insertAdjacentText("beforeend", activeButton.innerText);
+    } else if (oprCount === 1 && Number(mainLcd.innerText) === 0) {
+      mainLcd.innerText = activeButton.innerText;
+    } else if (oprCount === 1 && Number(mainLcd.innerText) !== 0) {
+      mainLcd.insertAdjacentText("beforeend", activeButton.innerText);
+    }
   }
+  // else if (hasDecimal) {
+  //   mainLcd.insertAdjacentText("beforeend", activeButton.innerText);
+  // }
 }
 
-// for decimal value
+// For decimal value =============
 function takeDecimal(activeID) {
-  checkdecimal();
   activeButton = document.getElementById(activeID);
+  checkDecimal();
+  if (hasDecimal) return;
+  console.log(hasDecimal);
   if (!hasDecimal) {
-    if (Number(mainLcd.innerText) === 0) {
+    if (Number(mainLcd.innerText) === 0 || mainLcd.innerText === "Welcome") {
       mainLcd.innerText = "0.";
-    } else if (mainLcd.innerText === "Welcome") {
-      mainLcd.innerText = `0.`;
+      hasDecimal = true;
     } else if (Number(mainLcd.innerText) !== 0) {
       mainLcd.insertAdjacentText("beforeend", ".");
+      hasDecimal = true;
     }
-    // checkDecimal = true;
-  } else if (hasDecimal) {
-    return -1;
   }
 }
 
+// Take operators ============
 function takeOperators(activeOprID) {
   hLength = historyScreen.innerText.length;
   activeOpr = document.getElementById(activeOprID);
-  if (oprCount !== 1) {
-    if (hLength === 1 && mainLcd.innerText !== `Welcome`) {
-      historyScreen.innerText = `${mainLcd.innerText} ${activeOpr.innerText}`;
-      hLength = historyScreen.innerText.length;
-    } else if (hLength > 1 && ansCount === 0) {
-      historyScreen.insertAdjacentText(
-        "beforeend",
-        `${mainLcd.innerText} ${activeOpr.innerText}`
-      );
-    } else if (ansCount === 1) {
-      historyScreen.innerText = `${mainLcd.innerText} ${activeOpr.innerText}`;
-    }
+
+  if (hLength === 1 && ansCount === 0 && mainLcd.innerText !== `Welcome`) {
+    historyScreen.innerText = `${mainLcd.innerText} ${activeOpr.innerText}`;
+    hLength = historyScreen.innerText.length;
+  } else if (hLength > 1 && ansCount === 0) {
+    historyScreen.insertAdjacentText(
+      "beforeend",
+      ` ${mainLcd.innerText} ${activeOpr.innerText}`
+    );
+  } else if (ansCount === 1) {
+    historyScreen.innerText = `${mainLcd.innerText} ${activeOpr.innerText}`;
   }
+
   mainLcd.innerText = "0";
   hLength = historyScreen.innerText.length;
   oprCount = 1;
 }
 
-// Answer button -------------
-
-const reveal = function () {
-  //console.log(`count = ${ansCount}`);
-  if (oprCount === 1) {
-    historyScreen.insertAdjacentText("beforeend", ` ${mainLcd.innerText}`);
-    problem = historyScreen.innerText;
-    //console.log(problem);
-    //console.log(eval(problem));
-    mainLcd.innerText = `${eval(problem)}`;
-  }
-  ansCount = 1;
-  oprCount = 0;
-};
-
+// Answer button ==========
 document.getElementById("answer").onclick = function showAnswer() {
-  //console.log(`count = ${ansCount}`);
   if (oprCount === 1) {
     historyScreen.insertAdjacentText("beforeend", ` ${mainLcd.innerText}`);
     problem = historyScreen.innerText;
-    //console.log(problem);
-    //console.log(eval(problem));
     mainLcd.innerText = `${eval(problem)}`;
   }
   ansCount = 1;
   oprCount = 0;
 };
 
-/*Installed GIT */
-/* added files to tracking for git */
-
-// adding decimal on aswers not working
-
-// Press Esc key to clear screen
+// Press Esc key to clear screen ===============
 document.addEventListener("keydown", function (event) {
   if (event.key === "Escape") clearThyScreen();
 });
